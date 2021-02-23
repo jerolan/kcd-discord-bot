05ac442f1a41dc3b9f8b69f11321fe2554194a8f
Jerome Olvera <11675668+jerolan@users.noreply.github.com>
2021-02-22 23:13:45 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const onboarding = require('./onboarding')
-const commands = require('./commands')
-const admin = require('./admin')
-const clubApplication = require('./club-application')
-const privateChat = require('./private-chat')
+// const onboarding = require('./onboarding')
+// const commands = require('./commands')
+// const admin = require('./admin')
+// const clubApplication = require('./club-application')
+// const privateChat = require('./private-chat')
 const rollbar = require('./rollbar')
 const meetup = require('./meetup')
 
@@ -16,10 +16,10 @@ function setup(client) {
 }
 
 module.exports = {
-  onboarding,
-  commands,
-  clubApplication,
+  // onboarding,
+  // commands,
+  // clubApplication,
   setup,
   rollbar,
-  admin,
+  // admin,
 }
